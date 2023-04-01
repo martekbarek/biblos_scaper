dd8bd3e98dc10c69c47849d0e3299b70c027b423
Bartosz Marek <bartek.marek40@gmail.com>
2023-04-01 17:02:21 +0200

    Create Foreign Key for Articles

--- scraper/biblos/views.py
+++ scraper/biblos/views.py
@@ -1,38 +1,43 @@
 from django.shortcuts import render, redirect
-from .scraper.scraper import get_articles_by_person, extract_data 
+from .scraper.scraper import get_articles_by_person, extract_data
 # Create your views here.
 from .models import *
 
 from django.http import HttpResponse
 
 
-
 def dashboard(request):
-    
-    # add list of already fetched persons' articles 
-    
-    return render(request, 'biblos/dashboard.html',dict())
+
+    # add list of already fetched persons' articles
+
+    return render(request, 'biblos/dashboard.html', dict())
 
 # change name to results
+
+
 def search(request):
     # page for results, there will be an button to redirect to main page
-    
+
     if request.method == 'POST':
         author = request.POST.get('author')
         raw_articles = get_articles_by_person(author)
-        articles=extract_data(raw_articles)
-        
+        articles = extract_data(raw_articles)
+
         for article in articles:
-            
-            # TODO:look for author ? create articles with him : create author
+
             query_name = [n for n in article.authors if author in n]
 
-            author_object = Author.objects.create(name=query_name[0])
-            Article.objects.create(author=author_object,title=article.title, release_data=article.release_data)
-        
-        print(request)
-        context = { "articles": articles }
-        
+            if Author.objects.filter(name=query_name).exists():
+                author_object = Author.objects.filter(name=query_name[0])
+            else:
+                author_object = Author.objects.create(name=query_name[0])
+
+            # concatenate exteranal authors with pk 
+            Article.objects.create(
+                author=author_object, title=article.title, release_data=article.release_data, typ=article.typ, series=article.series,points=article.points)
+
+        context = {"articles": articles}
+
     return render(request, 'biblos/search.html', context=context)
 
 
