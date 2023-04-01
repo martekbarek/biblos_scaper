37fbaea6b72293ac31bac1180ec40edec34efe07
Bartosz Marek <bartek.marek40@gmail.com>
2023-04-02 00:16:52 +0200

    Add error page and fix seraching bug

--- scraper/biblos/views.py
+++ scraper/biblos/views.py
@@ -8,8 +8,6 @@ from django.http import HttpResponse
 
 def dashboard(request):
 
-    # add list of already fetched persons' articles
-    
     authors = Author.objects.all()
     
     dict = {}
@@ -24,42 +22,41 @@ def dashboard(request):
     return render(request, 'biblos/dashboard.html', context={'dict' : dict})
 
 
-# change name to results
+# TODO:change name to results
 def search(request):
 
     if request.method == 'POST':
         author = request.POST.get('author')
-        # make author take surname and first letter
-        
-        raw_articles = get_articles_by_person(author)
-        articles = extract_data(raw_articles)
+        try:
+            raw_articles = get_articles_by_person(author)
+            articles = extract_data(raw_articles)
 
-        if not articles:
-            return render(request, 'biblos/dashboard.html')
-        
-        for article in articles:
-            try:
-                list_name = [n for n in article.authors if author in n][0].strip().split(',')
-                surname = list_name[0]
-                name = list_name[1].strip()[0]
-                query_name = surname + " " +name
-            except:
-                continue
-                
-            if Author.objects.filter(name__startswith=query_name).exists():
-                author_object = Author.objects.filter(name=query_name).first()
-            else:
-                author_object = Author.objects.create(name=query_name)
+            if not articles:
+                return render(request, 'biblos/dashboard.html')
+            
+            for article in articles:
+                try:
+                    list_name = [n for n in article.authors if author in n][0].strip().split(',')
+                    surname = list_name[0]
+                    name = list_name[1].strip()[0]
+                    query_name = surname + " " +name
+                except:
+                    continue
+                    
+                if Author.objects.filter(name__startswith=query_name).exists():
+                    author_object = Author.objects.filter(name=query_name).first()
+                else:
+                    author_object = Author.objects.create(name=query_name)
 
-            if not Article.objects.filter(title=article.title, author=author_object).exists():
-                Article.objects.create(author=author_object, authors=";".join(article.authors), ext_authors=";".join(article.ext_authors), title=article.title, release_data=article.release_data, typ=article.typ, series=article.series, points=article.points)
+                if not Article.objects.filter(title=article.title, author=author_object).exists():
+                    Article.objects.create(author=author_object, authors=";".join(article.authors), ext_authors=";".join(article.ext_authors), title=article.title, release_data=article.release_data, typ=article.typ, series=article.series, points=article.points)
+            
+            context = {"articles": articles}
+        except:
+            return render(request, 'biblos/error.html', context=dict({"query": author}))  
         
-        context = {"articles": articles}
-
     return render(request, 'biblos/search.html', context=context)
 
-
-# view for certain person data
 def author_view(request, id):
     
     author = Author.objects.filter(id=id).first()
