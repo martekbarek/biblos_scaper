043b52ceaa316226a8be516cf34b3845ae85fa58
Bartosz Marek <bartek.marek40@gmail.com>
2023-04-01 02:02:17 +0200

    Add views and .gitignore

--- scraper/biblos/views.py
+++ scraper/biblos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,5 +6,17 @@ from django.http import HttpResponse
 
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def dashboard(request):
+    
+    # add list of already fetched persons' articles 
+    
+    return render(request, 'biblos/dashboard.html',dict())
+
+def search(request):
+    # page for results, there will be an button to redirect to main page
+    
+    if request.method == 'POST':
+        # loading page and redirecting or task in the background
+        print(request)
+        
+    return render(request, 'biblos/search.html')
