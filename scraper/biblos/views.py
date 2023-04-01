d35a3fc53c001f66de1a4ba541192e8b90fca1c5
Bartosz Marek <bartek.marek40@gmail.com>
2023-04-01 16:42:02 +0200

    Save models to db and update .gitignore

--- scraper/biblos/views.py
+++ scraper/biblos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .scraper.scraper import get_articles_by_person, extract_data 
 # Create your views here.
+from .models import *
 
 from django.http import HttpResponse
 
@@ -20,7 +21,15 @@ def search(request):
         author = request.POST.get('author')
         raw_articles = get_articles_by_person(author)
         articles=extract_data(raw_articles)
-        # loading page and redirecting or task in the background
+        
+        for article in articles:
+            
+            # TODO:look for author ? create articles with him : create author
+            query_name = [n for n in article.authors if author in n]
+
+            author_object = Author.objects.create(name=query_name[0])
+            Article.objects.create(author=author_object,title=article.title, release_data=article.release_data)
+        
         print(request)
         context = { "articles": articles }
         
