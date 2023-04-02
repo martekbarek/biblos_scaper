127a23f385b07eb52eab6f824773d7200aa20b4f
Bartosz Marek <bartek.marek40@gmail.com>
2023-04-02 17:57:33 +0200

    Add delete button

--- scraper/biblos/urls.py
+++ scraper/biblos/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('search/', views.search, name='search'),
-    path('articles/<int:id>', views.author_view, name='articles')
+    path('articles/<int:id>', views.author_view, name='articles'),
+    path('delete/<int:id>', views.delete_articles_by_author, name='delete')
     
 ]
--- scraper/biblos/views.py
+++ scraper/biblos/views.py
@@ -1,27 +1,23 @@
 from django.shortcuts import render, redirect
 from .scraper.scraper import get_articles_by_person, extract_data
-# Create your views here.
 from .models import *
-
-from django.http import HttpResponse
-
 
 def dashboard(request):
 
     authors = Author.objects.all()
-    
+
     dict = {}
-    
+
     for author in authors:
         articles = Article.objects.filter(author=author, points__gt=0)
         points = 0
         for article in articles:
-            points+=article.points
+            points += article.points
         articles_count = articles.count()
-        if points>0:
-            dict.update({ author : [points,articles_count] })
+        if points > 0:
+            dict.update({author: [points, articles_count]})
 
-    return render(request, 'biblos/dashboard.html', context={'dict' : dict})
+    return render(request, 'biblos/dashboard.html', context={'dict': dict})
 
 
 # TODO:change name to results
@@ -35,39 +31,49 @@ def search(request):
 
             if not articles:
                 return render(request, 'biblos/dashboard.html')
-            
+
             for article in articles:
                 try:
-                    list_name = [n for n in article.authors if author in n][0].strip().split(',')
+                    list_name = [
+                        n for n in article.authors if author in n][0].strip().split(',')
                     surname = list_name[0]
                     name = list_name[1].strip()[0]
-                    query_name = surname + " " +name
+                    query_name = surname + " " + name
                 except:
                     continue
-                    
+
                 if Author.objects.filter(name__startswith=query_name).exists():
-                    author_object = Author.objects.filter(name=query_name).first()
+                    author_object = Author.objects.filter(
+                        name=query_name).first()
                 else:
                     author_object = Author.objects.create(name=query_name)
 
                 if not Article.objects.filter(title=article.title, author=author_object).exists():
-                    Article.objects.create(author=author_object, authors=";".join(article.authors), ext_authors=";".join(article.ext_authors), title=article.title, release_data=article.release_data, typ=article.typ, series=article.series, points=article.points)
-            
+                    Article.objects.create(author=author_object, authors=";".join(article.authors), ext_authors=";".join(
+                        article.ext_authors), title=article.title, release_data=article.release_data, typ=article.typ, series=article.series, points=article.points)
+
             context = {"articles": articles}
         except:
-            return render(request, 'biblos/error.html', context=dict({"query": author}))  
-        
+            return render(request, 'biblos/error.html', context=dict({"query": author}))
+
     return render(request, 'biblos/search.html', context=context)
 
+
 def author_view(request, id):
-    
+
     author = Author.objects.filter(id=id).first()
-        
+
     articles = Article.objects.filter(author=author, points__gt=0)
     points = 0
     for article in articles:
-        points+=article.points
-    context = dict({ "author" : author, "articles":articles, "points": points })
+        points += article.points
+    context = dict({"author": author, "articles": articles, "points": points})
 
     return render(request, 'biblos/author_view.html', context=context)
 
+
+def delete_articles_by_author(request, id):
+
+    Author.objects.filter(id=id).first().delete()
+
+    return redirect("/")
